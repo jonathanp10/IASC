085fb5cafdfa18965ecf3cde21348acca994bdd5
Jonathan <jonp10@gmail.com>
2021-04-21 10:03:56 +0300

    renamed iasc_X to X_rx_manager

---
+++ code/en/en_rx_manager.py
@@ -0,0 +1,58 @@
+import os, sys, inspect, logging, argparse
+
+# modify PYTHONPATH in order to imprt internal modules from parent directory.
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+from common.iasc_common import *
+from en.en_tx_manager import *
+
+def get_timestamp_lst(filename):
+    logging.debug("[{}][{}][Entered function] filename is {}".format(__name__, inspect.currentframe().f_code.co_name, filename))
+    original_filename = filename
+    filename.replace("rpi_lora_lte_records_", "")
+    filename = filename.replace(".csv", "")
+    filename = filename.replace('-', '.')
+    filename = filename.replace(':', '.')
+    filename = filename.replace('T', '.')
+    timestamp_lst = filename.split('.')
+    timestamp_lst.append(original_filename)
+    return timestamp_lst
+
+
+def get_pending_files_queue():
+    logging.debug("[{}][{}][Entered function]".format(__name__, inspect.currentframe().f_code.co_name))
+    # ignored_files = []
+    pending_files = []
+    timestamp_lsts = []
+    sorted_pending_files = []
+    for filename in os.listdir(pending_dir):
+        if filename in ignored_files or not filename.endswith(".csv"):
+            continue
+        else:
+            pending_files.append(filename)
+    for pending_file in pending_files:
+        timestamp_lst = get_timestamp_lst(pending_file)
+        timestamp_lsts.append(timestamp_lst)
+
+    timestamp_lsts.sort()
+
+    for lst in timestamp_lsts:
+        sorted_pending_files.append(lst[-1])
+    return sorted_pending_files
+
+
+def send_file_to_gw(filename, compression_mode):
+    logging.debug("[{}][{}][Entered function] filename is {}".format(__name__, inspect.currentframe().f_code.co_name, filename))
+    send_file_to_gw_with_lora(filename, compression_mode)
+
+def run_rx(compression_mode):
+    ignored_lst = []
+    while True:
+        pending_files_queue = get_pending_files_queue()
+        logging.info("[{}]: The sorted pending queue is: {}".format(__name__, str(pending_files_queue)))
+        for pending_file in pending_files_queue:
+            if pending_file in ignored_lst:
+                continue
+            send_file_to_gw(pending_file, compression_mode)
+            ignored_lst.append(pending_file)
--- code/gw/iasc_gw.py
+++ code/gw/iasc_gw.py
@@ -1,4 +1,4 @@
-import os, sys, inspect, logging, lzma
+import os, sys, inspect, logging
 
 # modify PYTHONPATH in order to imprt internal modules from parent directory.
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -6,84 +6,16 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
 from common.iasc_common import *
-from gw.gw_tx_manager import *
-from gw.cloud_status import check_success
+from gw.gw_rx_manager import run_rx
 
-compression_mode = True
 
-ignore_list = []
 if __name__ == "__main__":
     LOG_FILE_NAME = "iasc_gw.log"
     logging.basicConfig(filename=LOG_FILE_NAME, filemode='w', level=logging.INFO, format="%(asctime)s:%(levelname)-8s %(message)s")
-
-
-def get_id_from_filename(filename):
-    logging.debug("[{}][{}][Entered function]".format(__name__, inspect.currentframe().f_code.co_name))
-    return filename.replace('./', '').split('_')[0]
-
-
-def pseudo_recieve():
-    logging.debug("[{}][{}][Entered function]".format(__name__, inspect.currentframe().f_code.co_name))
-    os.chdir(en_gw_bridge_dir)
-    rx_msgs = filter(os.path.isfile, os.listdir('.'))
-    if len(rx_msgs) == 0:
-        return "__EMPTY_DIR__", -1
-    rx_msgs = [os.path.join('.',f) for f in rx_msgs]
-    rx_msgs.sort(key=lambda x: os.path.getmtime(x))
-    # print("rx msgs:\n " + str(rx_msgs))
-    curr_file = open(rx_msgs[0], 'rb')
-    curr_msg = curr_file.read()
-    curr_file.close()
-    os.remove(rx_msgs[0])
-    os.chdir(working_dir)
-    return curr_msg, get_id_from_filename(rx_msgs[0])
-
-
-def extract_metadata(metadata):
-    logging.debug("[{}][{}][Entered function]".format(__name__, inspect.currentframe().f_code.co_name + "metadata is: " + metadata))
-    metadata_lst = metadata.split('.csv_')
-    filename = metadata_lst[0] + '.csv'
-    first, last, sequence_num = metadata_lst[1].split('_')
-    return first=='1', last=='1', int(sequence_num), filename
-
-
-def upload_to_cloud(filepath):
-    logging.debug("[{}][{}][Entered function]".format(__name__, inspect.currentframe().f_code.co_name + "filepath is: " + filepath))
-    send_file_to_aws(filepath)
-
-###################################################
-###################  MAIN   #######################
-###################################################
-logging.info("[{}] Starting run...]".format(__name__))
-while True:
-    # msg = recieve()
-    msg, source_id = pseudo_recieve()
-    if msg == "__EMPTY_DIR__":
-        logging.info("[{}][COMPRESSION_MODE:{}] bridge_dir is empty".format(__name__,str(compression_mode)))
-        break
-    first, last, sequence_num, filename = extract_metadata(msg.split('\n')[0])
-    msg_data = "\n".join(msg.split('\n')[1::])
-    logging.info("[{}] msg_metadata: {} {} {} {} id {} \n".format(__name__, filename, str(first), str(last), str(sequence_num), source_id, msg_data))
-    logging.debug("[{}] MsgData: {} \n".format(__name__, msg_data))
-    filepath = gw_queues_dir + '/' + filename
-
-    # first msg - create new file
-    if first:
-        curr_en_queue = open(filepath, 'wb+')
-        chunk = b''
-    curr_en_queue.write(msg_data)
-    # last msg - close file and upload to cloud
-    if last:
-        if compression_mode:
-            curr_en_queue.seek(0)
-            compressed_data = curr_en_queue.read()
-            print("DEBUG: Received compressed data of {} Bytes\n".format(len(compressed_data)))
-            data = lzma.decompress(compressed_data)
-            print("DEBUG: Decompressed data size: {} Bytes\n".format(len(data)))
-            curr_en_queue.seek(0)
-            curr_en_queue.write(data)
-        curr_en_queue.close()
-        upload_to_cloud(filepath)
-        if check_success(filename):
-            print("Something went wrong... output file is not identical to original file.\n")
-
+    
+    # initializations
+    ignored_lst = []
+    compression_mode = True
+    
+    # run gw rx
+    run_rx(compression_mode)
